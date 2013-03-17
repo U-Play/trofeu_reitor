f7d49c18773e94d69f173eac32acf74fe181861f
Miguel Palhas <mpalhas@gmail.com>
2013-03-17 23:51:30 +0000

    now allows basic registration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,6 @@ Uplay::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,11 @@ ActiveRecord::Schema.define(:version => 20130317225852) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "password_salt"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "first_name"
