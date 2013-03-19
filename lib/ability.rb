cc243e81e1198c941facf214c24e04f7c99885bd
Miguel Palhas <mpalhas@gmail.com>
2013-03-19 17:21:20 +0000

    login/logout flow now working, also for admin area

--- lib/ability.rb
+++ lib/ability.rb
@@ -2,6 +2,18 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    send user.role.name unless user.nil?
+  end
+
+  def root
+    admin
+  end
+
+  def admin
+    can :access, :admin
+  end
+
+  def athlete
   end
 
 end
