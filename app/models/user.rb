f68b84e57a57c6d4cb0d5efbd93a9a5dc14c5453
Miguel Palhas <mpalhas@gmail.com>
2013-03-19 02:33:08 +0000

    added user#picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+                  :first_name, :last_name, :username, :course, :student_number, :sports_number, :picture
+
+  has_attached_file :picture
+                    # TODO check this configs when possible
+                    #styles: { medium: "300x300>", thumb: "100x100>" },
+                    #default_url: "/images/:style/missing.png"
 end
