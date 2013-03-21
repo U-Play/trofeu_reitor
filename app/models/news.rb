1651147a1cc4f6d608bdb5b3e8240d3d984a9575
Bruno Azevedo <azevedo.252@gmail.com>
2013-03-21 00:37:11 +0000

    altered relation name

--- app/models/news.rb
+++ app/models/news.rb
@@ -13,7 +13,7 @@ class News < ActiveRecord::Base
   has_many :tournaments, through: :news_references, :source => :newsable, :source_type => 'Tournament'
   has_many :teams, through: :news_references, :source => :newsable, :source_type => 'Team'
   has_many :matches, through: :news_references, :source => :newsable, :source_type => 'Match'
-  has_many :athletes, through: :news_references, :source => :newsable, :source_type => 'User'
+  has_many :users, through: :news_references, :source => :newsable, :source_type => 'User'
 
   ## Attributes ##
   attr_accessible :deleted_at
