87633a78a8dcdfecbeb93f159f9b6a7a8a704d22
Gabriel Poça <gabrielpoca@gmail.com>
2013-04-20 20:05:17 +0100

    added seeds task

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,4 +32,11 @@ namespace :deploy do
 	end
 end
 
+namespace :deploy do
+    desc "reload the database with seed data"
+    task :seed do
+      run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+    end
+end
+
 after 'deploy:update_code', 'deploy:migrate'
