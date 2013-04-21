09dece760dd383bfcdd47c3c13cdd6a8ee601090
Miguel Palhas <mpalhas@gmail.com>
2013-04-21 01:23:58 +0100

    changed default admin_mailer destination

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,9 +1,10 @@
 class AdminMailer < ApplicationMailer
   default from: "noreply@uplay.com"
-  #default to: "admin@uplay.com" # TODO definir aqui o mail do admin
+  default to: "test@uplaypro.com" # TODO definir aqui o mail do admin
 
   def validation_requested(user)
     @user = user
-    mail to: User.by_role('admin').map(&:email), subject: "Troféu Reitor 2013 - Validação de Atleta"
+    #mail to: User.by_role('admin').map(&:email), subject: "Troféu Reitor 2013 - Validação de Atleta"
+    mail subject: "Troféu Reitor 2013 - Validação de Atleta"
   end
 end
