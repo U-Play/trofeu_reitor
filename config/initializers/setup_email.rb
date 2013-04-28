768119d3294444161dbcc0b846af26ba75852f8c
Miguel Palhas <mpalhas@gmail.com>
2013-04-28 01:27:23 +0100

    updates to setup_email.rb

--- config/initializers/setup_email.rb
+++ config/initializers/setup_email.rb
@@ -2,24 +2,25 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.perform_deliveries = true
 
 if Rails.env.production?
-  file = '/home/deploy/config/accounts.yml'
+  file = File.open '/home/deploy/config/accounts.yml'
 else
-  file = File.join(Rails.root, 'config', 'accounts.yml')
+  filename = File.join(Rails.root, 'config', 'accounts.yml')
+  file = File.open filename if File.exists?(filename)
+
+  ActionMailer::Base.default_url_options[:port] = 3000
 end
 
-accounts  = YAML::load(File.open(file)).to_hash
-mail_account = accounts['trofeu_reitor']['mail'][Rails.env.to_s]
-
-
-ActionMailer::Base.smtp_settings = {
-  user_name: mail_account['username'],
-  password:  mail_account['password'],
-  domain:    mail_account['domain'],
-  address: 'smtp.gmail.com',
-  port: 587,
-  authentication: 'plain',
-  enable_starttls_auto: true
-}
-
-ActionMailer::Base.default_url_options[:host] = mail_account['host']
-ActionMailer::Base.default_url_options[:port] = 3000 unless Rails.env.production?
+if file
+  accounts  = YAML::load(file).to_hash
+  mail_account = accounts['trofeu_reitor']['mail'][Rails.env.to_s]
+  ActionMailer::Base.default_url_options[:host] = mail_account['host']
+  ActionMailer::Base.smtp_settings = {
+    user_name: mail_account['username'],
+    password:  mail_account['password'],
+    domain:    mail_account['domain'],
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+end
