a47c1f2b458296adc4413cbbe5e7ca21845b444b
Cedric Pimenta <cedricpim@gmail.com>
2013-05-06 20:34:07 +0100

    small fix to correct links

--- app/admin/tournaments.rb
+++ app/admin/tournaments.rb
@@ -77,7 +77,7 @@ ActiveAdmin.register Tournament do
   end
 
   action_item :only => :show, :if => proc{ tournament.group_stage && !tournament.group_stage.draft_made?} do
-    link_to 'Groups Draft', :controller => "tournaments", :action => "groups_draft", :id => tournament.id
+    link_to 'Groups Draft', :controller => 'tournaments', :action => 'groups_draft', :id => tournament.id
   end
 
   member_action :groups_draft do
@@ -91,7 +91,7 @@ ActiveAdmin.register Tournament do
   end
 
   action_item :only => :show, :if => proc{ tournament.knockout_stage && !tournament.knockout_stage.draft_made? } do 
-    link_to 'Knockout Draft', :controller => "tournaments", :action => "knockout_draft", :id => tournament.id
+    link_to 'Knockout Draft', :controller => 'tournaments', :action => 'knockout_draft', :id => tournament.id
   end 
 
   #Action to show the page where the admin can do the manual draft
@@ -118,12 +118,12 @@ ActiveAdmin.register Tournament do
       if selected_teams.length == @tournament.number_of_teams && !game_not_selected
         @tournament.knockout_stage.set_exempt_winners
         @tournament.knockout_stage.update_attributes(:draft_made => true)
-        redirect_to admin_tournament_path(@tournament), :notice => "Draft completed"
+        redirect_to admin_tournament_path(@tournament), :notice => 'Draft completed'
       else
-        redirect_to admin_tournament_path(@tournament), :alert => "There are matches without one team at least"
+        redirect_to admin_tournament_path(@tournament), :alert => 'There are matches without one team at least'
       end
     else
-      @tournament.errors[:base] << "The same team cannot be selected for two different matches"
+      @tournament.errors[:base] << 'The same team cannot be selected for two different matches'
       @teams = @tournament.teams
       @first_stage = @tournament.knockout_stage.number_of_stages - 1
       render :knockout_draft
@@ -131,18 +131,19 @@ ActiveAdmin.register Tournament do
   end
 
   action_item :only => :show, :if => proc{ tournament.knockout_stage && !tournament.knockout_stage.draft_made? && can?(:manage, tournament) } do 
-    link_to 'Knockout Draft', :controller => "tournaments", :action => "show_knockout_draft", :id => tournament.id
+    link_to 'Knockout Draft', :controller => 'tournaments', :action => 'knockout_draft', :id => tournament.id
   end 
 
   #Action to show the page where the admin can do the manual draft
-  member_action :show_knockout_draft, :method => :get do
+  member_action :knockout_draft, :method => :get do
     @tournament = Tournament.find(params[:id])
     authorize! :manage, @tournament
     @teams = @tournament.teams
+    @first_stage = @tournament.knockout_stage.number_of_stages - 1
   end
 
   action_item :only => :show, :if => proc{ tournament.has_minimum_teams? && !tournament.began? && can?(:manage, tournament) } do 
-    link_to 'Begin Tournament', :controller => "tournaments", :action => "final_configuration", :id => tournament.id
+    link_to 'Begin Tournament', :controller => 'tournaments', :action => 'final_configuration', :id => tournament.id
   end 
 
   member_action :final_configuration, :method => :get do
@@ -166,7 +167,7 @@ ActiveAdmin.register Tournament do
   end
 
   action_item :only => :show, :if => proc{ !tournament.knockout_stage.nil? && tournament.knockout_stage.actual_stage_finished? && can?(:manage, tournament) } do
-    link_to 'Generate Next Stage', {:controller => "tournaments", :action => "next_stage", :id => tournament.id}, :method => :post
+    link_to 'Generate Next Stage', {:controller => 'tournaments', :action => 'next_stage', :id => tournament.id}, :method => :post
   end
 
   #Action that will update the matches with teams that go to the next stage
